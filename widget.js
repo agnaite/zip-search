a53d55f9b64f272085acddf4f0e8b96869cf64d9
agnė <agne@untz.club>
2016-02-10 15:31:51 -0800

    fixed some formatting

--- widget.js
+++ widget.js
@@ -1,6 +1,8 @@
+"use strict";
+
 //checks if user input is a 5 digit zip code
 function isValidZip(input) {
-   	return /^\d{5}$/.test(input);
+  return (/^\d{5}$/).test(input);
 }
 
 //on click, creates http request and parses json file if file is found
@@ -9,24 +11,24 @@ document.getElementById("submit").addEventListener("click", function () {
 	document.getElementById("city").value = "";
 	document.getElementById("state").value = "";
 	document.getElementById("error").innerHTML = "";
-	
-	if (isValidZip(input)) {
-		var url = "postalcodes/"+ input +".json";
 
+	if(isValidZip(input)) {
+		var url = "postalcodes/"+ input +".json";
 		var xmlhttp = new XMLHttpRequest();
 
 		xmlhttp.onreadystatechange = function() {
-		    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		        var data = JSON.parse(xmlhttp.responseText);
-		        zipSearch(data);
-		    }
+		  if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+		    var data = JSON.parse(xmlhttp.responseText);
+		    zipSearch(data);
+		  }
 		    //zip code not found error 
-		    else if (xmlhttp.status == 404) {
-		    	document.getElementById("error").innerHTML = "ZIP code " + input + " was not found.";
-		    	document.getElementById("zip").value = "";
-		    }
+		  else if(xmlhttp.status === 404) {
+		    document.getElementById("error").innerHTML = "ZIP code " + input + " was not found.";
+		    document.getElementById("zip").value = "";
+		  }
 		};
-		xmlhttp.open("GET", url, true);
+		
+    xmlhttp.open("GET", url, true);
 		xmlhttp.send();
 
 		//writes city and state from json file 
@@ -39,7 +41,7 @@ document.getElementById("submit").addEventListener("click", function () {
 	}
 	else {
 		//incorrect user entry error
-		document.getElementById("error").innerHTML = "\"" + input + "\"" + " is not a valid ZIP code. Please enter again (e.g. 32601).";
+		document.getElementById("error").innerHTML = "\"" + input + "\"" + " is not a valid ZIP code. Please enter again (e.g. 02120).";
 		document.getElementById("zip").value = "";
 	}
 
