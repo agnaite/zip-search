5f3e3c46a13af5bcdd534a819443c55d7cb32582
agnė <agne@untz.club>
2016-02-10 18:35:33 -0800

    fixed indentation

--- widget.js
+++ widget.js
@@ -2,35 +2,35 @@
 
 //on click, creates http request and parses json file if file is found
 document.getElementById("submit").addEventListener("click", function () {
-	var input = document.getElementById("zip").value;
-	document.getElementById("city").value = "";
-	document.getElementById("state").value = "";
-	document.getElementById("error").innerHTML = "";
+  var input = document.getElementById("zip").value;
+  document.getElementById("city").value = "";
+  document.getElementById("state").value = "";
+  document.getElementById("error").innerHTML = "";
 
-	if(isValidZip(input)) {
-		var url = "postalcodes/"+ input +".json";
-		var xmlhttp = new XMLHttpRequest();
+  if(isValidZip(input)) {
+    var url = "postalcodes/"+ input +".json";
+    var xmlhttp = new XMLHttpRequest();
 
-		xmlhttp.onreadystatechange = function() {
-		  if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-		    var data = JSON.parse(xmlhttp.responseText);
-		    zipSearch(data);
-		  }
-		    //zip code not found error 
-		  else if(xmlhttp.status === 404) {
-		    document.getElementById("error").innerHTML = "ZIP code " + input + " was not found.";
-		    document.getElementById("zip").value = "";
-		  }
-		};
-		
+    xmlhttp.onreadystatechange = function() {
+      if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+        var data = JSON.parse(xmlhttp.responseText);
+        zipSearch(data);
+      }
+        //zip code not found error 
+      else if(xmlhttp.status === 404) {
+        document.getElementById("error").innerHTML = "ZIP code " + input + " was not found.";
+        document.getElementById("zip").value = "";
+      }
+    };
+    
     xmlhttp.open("GET", url, true);
-		xmlhttp.send();
-	}
-	else {
-		//incorrect user entry error
-		document.getElementById("error").innerHTML = "\"" + input + "\"" + " is not a valid ZIP code. Please enter again (e.g. 02120).";
-		document.getElementById("zip").value = "";
-	}
+    xmlhttp.send();
+  }
+  else {
+    //incorrect user entry error
+    document.getElementById("error").innerHTML = "\"" + input + "\"" + " is not a valid ZIP code. Please enter again (e.g. 02120).";
+    document.getElementById("zip").value = "";
+  }
 
 });
 
